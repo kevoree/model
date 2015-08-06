e29cee01d0eed77b71cffd72fe1c32cd4929ff70
Maxime Tricoire <max.tricoire@gmail.com>
2015-08-06 14:16:09 +0200

    update

--- src/test/java/org/kevoree/test/SimpleTest.java
+++ src/test/java/org/kevoree/test/SimpleTest.java
@@ -2,9 +2,9 @@ package org.kevoree.test;
 
 import org.KevoreeModel;
 import org.junit.Test;
-import org.kevoree.Node;
 import org.kevoree.Model;
-import org.kevoree.modeling.KCallback;
+import org.kevoree.Node;
+import org.kevoree.modeling.memory.manager.DataManagerBuilder;
 
 /**
  * Created by duke on 14/01/15.
@@ -13,33 +13,16 @@ public class SimpleTest {
 
     @Test
     public void helloTest() {
-
-
-        KevoreeModel model = new KevoreeModel();
-        model.connect(new KCallback() {
-            @Override
-            public void on(Object o) {
-
-            }
-        });
+        KevoreeModel model = new KevoreeModel(DataManagerBuilder.buildDefault());
+        model.connect(null);
         Model root = model.universe(0).time(0).createModel();
-        model.universe(0).time(0).setRoot(root, new KCallback() {
-            @Override
-            public void on(Object o) {
-
-            }
-        });
+        model.universe(0).time(0).setRoot(root, null);
 
         Node node = model.universe(0).time(0).createNode();
         node.setName("node0");
         root.addNodes(node);
 
-        model.save(new KCallback() {
-            @Override
-            public void on(Object o) {
-
-            }
-        });
+        model.save(null);
 
     }
 
